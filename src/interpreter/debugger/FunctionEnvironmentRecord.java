cec1d134e37575c54d72acdf0b60b4896af40c8d
Raskolnikov <m.cadier.kim@gmail.com>
2014-04-11 17:41:40 -0700

    Readded files

---
+++ src/interpreter/debugger/FunctionEnvironmentRecord.java
@@ -0,0 +1,160 @@
+
+package interpreter.debugger;
+
+/**
+ *
+ * @author admin
+ */
+public class FunctionEnvironmentRecord {
+    private SymbolTable table;
+    private String functName;
+    private Integer startLine;
+    private Integer endLine;
+    private Integer currentLine;
+    
+    
+    public static void main(String args[]){
+        FunctionEnvironmentRecord record = new FunctionEnvironmentRecord();
+        FunctionEnvironmentRecordUnitTests.test_1(record);
+    }
+    
+    
+    public FunctionEnvironmentRecord(){
+        table = new SymbolTable();
+    }
+    
+    public void setFunction(String name,int start, int end){
+        functName = name;
+        startLine = start;
+        endLine = end;
+    }
+    
+    
+    public void setStartLine(int n){
+        startLine = n;
+    }
+    
+    public int getStartLine(){
+        return startLine;
+    }
+
+    public void setEndLine(int n){
+        endLine = n;
+    }
+    
+    public int getEndLine(){
+        return endLine;
+    }
+    
+    public void setCurrentLine(int n){
+        currentLine = n;
+    }
+    
+    public int getCurrentLine(){
+        return currentLine;
+    }
+    
+    public void beginScope(){
+        table.beginScope();
+    }
+    
+    
+    public void enter(String key, int value){
+        table.put(key, value);
+        
+    }
+    
+    public void pop(int n){
+        table.pop(n);
+    }
+    
+    
+    public String dump(){
+        return "(" + this.printTable() + "," + 
+                (functName!=null ? functName : "-")+
+                "," + 
+                (startLine!=null ?startLine :"-") +
+                "," + 
+                (endLine!=null ?endLine :"-")+
+                "," + 
+                (currentLine!=null ?currentLine :"-") + 
+                ")";
+    }
+    
+    // Returns a string of the key value pairs <a/1,b/0,...>
+    public String printTable(){
+        String tableState = "<";
+        java.util.Set<String> keys =table.keys() ;
+        for (String key : keys ){
+            tableState += key+ "/" + table.get(key)+",";
+        }
+        // take of trailing comma. 
+        if ( tableState.length()>1) tableState = tableState.substring(0, tableState.length()-1);
+        tableState+= ">";
+                
+        return tableState;
+    }
+    
+}
+
+
+class FunctionEnvironmentRecordUnitTests{
+    
+    
+    /**
+     * Tests the following series of commands: 
+     * 
+     * BS				Begin scope
+     * Function g 1 20	Set the fields for function name, start and end
+     * Line 5				Set the current line to n
+     * Enter a 4  		Enter the var/value pair in the symbol table
+     * Enter b 2
+     * Enter c 7
+     * Enter i 3
+     * Pop 1				Remove the last n var/value pairs from the symbol table
+     * POP 2
+     * POP 1
+     * 
+     * @param record
+     * @return 
+     */
+    public static void test_1(FunctionEnvironmentRecord record){
+        System.out.println("BS");
+        record.beginScope();
+        
+        System.out.println("Function g 1 20");
+        record.setFunction("g", 1, 20);
+        
+        System.out.println("Line 5");
+        record.setCurrentLine(5);
+        
+        System.out.print("Enter a 4 \t");
+        record.enter("a", 4);       
+        System.out.println(record.dump());
+        
+        System.out.print("Enter b 2 \t");
+        record.enter("b", 2);       
+        System.out.println(record.dump());
+        
+        System.out.print("Enter c 7 \t");
+        record.enter("c", 7);       
+        System.out.println(record.dump());
+        
+        System.out.print("Enter a 1 \t");
+        record.enter("a", 1);       
+        System.out.println(record.dump());
+        
+        System.out.print("Pop 2     \t");
+        record.pop(2);
+        System.out.println(record.dump());
+        
+         System.out.print("POP 1     \t");
+        record.pop(1);
+        System.out.println(record.dump());
+        
+    }
+    
+    
+    
+    
+}
---
+++ src/interpreter/debugger/SymbolTable.java
@@ -0,0 +1,154 @@
+
+package interpreter.debugger;
+
+/** <pre>
+ *  Binder objects group 3 fields
+ *  1. a value
+ *  2. the next link in the chain of symbols in the current scope
+ *  3. the next link of a previous Binder for the same identifier
+ *     in a previous scope
+ *  </pre>
+*/
+class Binder {
+  private Object value;
+  private String prevtop;   // prior symbol in same scope
+  private Binder tail;      // prior binder for same symbol
+                            // restore this when closing scope
+  Binder(Object v, String p, Binder t) {
+	value=v; prevtop=p; tail=t;
+  }
+
+  Object getValue() { return value; }
+  String getPrevtop() { return prevtop; }
+  Binder getTail() { return tail; }
+}
+
+
+/** <pre>
+ * The Table class is similar to java.util.Dictionary, except that
+ * each key must be a String and there is a scope mechanism.
+ *
+ * Consider the following sequence of events for table t:
+ * t.put(String("a"),5)
+ * t.beginScope()
+ * t.put(String("b"),7)
+ * t.put(String("a"),9)
+ * 
+ * symbols will have the key/value pairs for Strings "a" and "b" as:
+ * 
+ * String("a") ->
+ *     Binder(9, String("b") , Binder(5, null, null) )
+ * (the second field has a reference to the prior String added in this
+ * scope; the third field refers to the Binder for the String("a")
+ * included in the prior scope)
+ * Binder has 2 linked lists - the second field contains list of symbols
+ * added to the current scope; the third field contains the list of
+ * Binders for the Strings with the same string id - in this case, "a"
+ * 
+ * String("b") ->
+ *     Binder(7, null, null)
+ * (the second field is null since there are no other symbols to link
+ * in this scope; the third field is null since there is no String("b")
+ * in prior scopes)
+ * 
+ * top has a reference to String("a") which was the last symbol added
+ * to current scope
+ * 
+ * Note: What happens if a symbol is defined twice in the same scope??
+ * </pre>
+*/
+public class SymbolTable {
+
+  private java.util.HashMap<String,Binder> symbols = new java.util.HashMap<String,Binder>();
+  private String top;    // reference to last symbol added to
+                         // current scope; this essentially is the
+                         // start of a linked list of symbols in scope
+  private Binder marks;  // scope mark; essentially we have a stack of
+                         // marks - push for new scope; pop when closing
+                         // scope
+
+/*
+    public static void main(String args[]) {
+        String s = String.symbol("a", 1),
+            s1 = String.symbol("b", 2),
+            s2 = String.symbol("c", 3);
+
+        Table t = new Table();
+        t.beginScope();
+        t.put(s,"top-level a");
+        t.put(s1,"top-level b");
+        t.beginScope();
+        t.put(s2,"second-level c");
+        t.put(s,"second-level a");
+        t.endScope();
+        t.put(s2,"top-level c");
+        t.endScope();
+}
+
+*/
+  public SymbolTable(){}
+
+
+ /**
+  * Gets the object associated with the specified symbol in the Table.
+  */
+  public Object get(String key) {
+	Binder e = symbols.get(key);
+	return e.getValue();
+  }
+  
+  
+
+ /**
+  * Puts the specified value into the Table, bound to the specified String.<br>
+  * Maintain the list of symbols in the current scope (top);<br>
+  * Add to list of symbols in prior scope with the same string identifier
+  */
+  public void put(String key, Object value) {
+	symbols.put(key, new Binder(value, top, symbols.get(key)));
+	top = key;
+  }
+
+ /**
+  * Remembers the current state of the Table; push new mark on mark stack
+  */
+  public void beginScope() {
+    marks = new Binder(null,top,marks);
+    top=null;
+  }
+
+ /**
+  * Restores the table to what it was at the most recent beginScope
+  *	that has not already been ended.
+  */
+  public void endScope() {
+	while (top!=null) {
+	   Binder e = symbols.get(top);
+	   if (e.getTail()!=null) symbols.put(top,e.getTail());
+	   else symbols.remove(top);
+	   top = e.getPrevtop();
+	}
+	top=marks.getPrevtop();
+	marks=marks.getTail();
+  }
+  
+  
+  // pops the last n entries into the table. 
+  public void pop(int n){
+      for(int i = 0; i<n; i++){
+        Binder topBinder = symbols.get(top);
+        if (topBinder.getTail() != null){
+            symbols.put(top, topBinder.getTail());
+        } else {
+            symbols.remove(top);
+        }
+        top = topBinder.getPrevtop();
+      }
+  }
+  
+  
+  /**
+   * @return a set of the Table's symbols.
+   */
+  public java.util.Set<String> keys() {return symbols.keySet();}
+}
