eb17e1fde588d143c8ec3c4c70890a1aa724409d
Raskolnikov <m.cadier.kim@gmail.com>
2014-04-11 17:39:03 -0700

    Added tests for FunctionEnvironmentRecord.

---
+++ src/interpreter/debugger/Debugger.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interpreter.debugger;
+
+/**
+ *
+ * @author admin
+ */
+public class Debugger {
+    
+}
