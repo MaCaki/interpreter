a1ee9038d342042ac69595fc6fe657a8939b6739
Matthew Kim <m.cadier.kim@gmail.com>
2014-04-22 03:18:29 -0700

    last save to debugvm

--- src/interpreter/debugger/DebuggerVirtualMachine.java
+++ src/interpreter/debugger/DebuggerVirtualMachine.java
@@ -25,7 +25,27 @@ public class DebuggerVirtualMachine extends VirtualMachine{
     // returnAddrs stack. 
     // Every ByteCode object should have an exectute( VirtualMacine ) method. 
     // 
-    
+    public void executeProgram(){
+        pc = 0;
+        runStack = new RunTimeStack();
+        returnAddrs = new Stack();
+        isRunning = true;
+        BinaryOpTable.init();
+        
+        // VM should be responsible for dumping to Console. 
+        
+        while (isRunning){
+            ByteCode code = program.getCode(pc);
+            code.execute(this);
+            // only dump if the current code is not DUMP and dumping is set. 
+            if (!code.getClass().getName().equals("interpreter.ByteCode.DumpByteCode")
+                    && dumping){
+                System.out.println(code.toString());
+                runStack.dump();
+            }
+            pc++;
+        }
+    }
     
     public void setLineNumber(int n){
         lineNumber = n;
